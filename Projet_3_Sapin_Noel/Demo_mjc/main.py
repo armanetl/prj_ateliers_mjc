6fc947d1d4e92bd2b209dde2dc5ca85e8d3dc1d0
armanetl <crocfrisk38@hotmail.fr>
2025-05-18 20:32:07 +0200

    Nettoyage

--- Projet_3_Sapin_Noel/Demo_mjc/main.py
+++
@@ -1,7 +0,0 @@
-from main_demo_leds_neopixel_2_set_pixel import *
-from main_demo_2025_music_leds import *
-
-
-
-main_demo_leds_neopixel_2_set_pixel()
-main_demo_2025_music_leds()
--- Projet_5_Robots/main.py
+++
@@ -1,9 +0,0 @@
-
-#from main_exemple_detection_objet import *
-#main_exemple_detection_objet()
-
-#from main_exemple_deplacement_robot import *
-#main_exemple_deplacement_robot()
-
-from main_exemple_telecommande import *
-main_exemple_telecommande()
